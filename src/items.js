7c564af620a580c22d0c86f7ac9f935dde312603
yarism <johansson.joakim@gmail.com>
2026-09-04 16:40:00 +0200

    Remove items
    Removing something that is not there does nothing.

--- src/items.js
+++ src/items.js
@@ -3,3 +3,8 @@ export const items = [];
 export function addItem(name, amount) {
   items.push({ name, amount });
 }
+
+export function removeItem(name) {
+  const i = items.findIndex((item) => item.name === name);
+  if (i >= 0) items.splice(i, 1);
+}
